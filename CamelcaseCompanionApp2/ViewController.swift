516387454cf918d665ecaa992be20f965f5ff758
Daniel Storek <dstorek@post.cz>
2014-11-01 10:37:09 +0100

    Initial Commit

---
+++ CamelcaseCompanionApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CamelcaseCompanionApp2
+//
+//  Created by Daniel Storek on 01/11/14.
+//  Copyright (c) 2014 Daniel Storek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
